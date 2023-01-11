23542c0c5295dccc5dc19fa097121194beeb4c1b
Geno Aton <60146621+tatski32@users.noreply.github.com>
2023-01-11 11:40:43 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,3 +5,16 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+const weekday = [
+"Sunday - Closed",
+"Monday - Open today 09:00am - 05:00pm",
+"Tuesday - Open today 09:00am - 05:00pm",
+"Wednesday - Open today 09:00am - 05:00pm",
+"Thursday - Open today 09:00am - 05:00pm",
+"Friday - Open today 09:00am - 05:00pm",
+"Saturday - Closed"];
+
+const d = new Date();
+let day = weekday[d.getDay()];
+document.getElementById("demo").innerHTML = day;
