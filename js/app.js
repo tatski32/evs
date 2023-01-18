2ae9f2f4400f999dc8163c3ff021531351929703
Geno Aton <60146621+tatski32@users.noreply.github.com>
2023-01-18 16:59:21 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,6 +7,11 @@ menu.addEventListener('click', function() {
 });
 
 
+
+
+
+
+
 const weekday = [
 "Sunday",
 "Monday",
@@ -34,3 +39,51 @@ const t = new Date();
 let time = timely[t.getDay()];
 document.getElementById("daytime").innerHTML = time;
 
+
+
+
+
+
+
+
+// Some random colors
+const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+
+const numBalls = 25;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
